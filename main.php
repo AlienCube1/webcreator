8a898db1bd166d6acb1c267e9d82af4bcddca473
AlienCube1 <marcelbockovac1@gmail.com>
2019-07-04 14:27:38 +0200

    Actually adding main.php beacause I forgot the last time...

---
+++ main.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<form method='post'>
+<input type='text' name = 'title'>
+<label for="title">Enter title</label>
+<input type='text' name = 'header'>
+<label for='header'>Enter header</label> 
+<input type='text' name = 'paragraph'
+<label for='paragraph'>Enter a paragraph</form>
+<input type='submit' name = 'submit'>
+
+
+
+</form>
+
+
+
+</body>
+</html>
